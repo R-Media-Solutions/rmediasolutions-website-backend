67cdda2b51895de320963a122c5f112a803ba21f
rifal <radenrifalardiansyah@gmail.com>
2022-12-14 10:31:21 +0700

    Update Router

--- main.go
+++ main.go
@@ -16,17 +16,16 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/aboutus", aboutuscontroller.Index).Methods("GET")
-	r.HandleFunc("/aboutus", aboutuscontroller.Index).Methods("GET")
 	r.HandleFunc("/aboutus/{id}", aboutuscontroller.Show).Methods("GET")
-	r.HandleFunc("/aboutus", aboutuscontroller.Create).Methods("POST")
-	r.HandleFunc("/aboutus/{id}", aboutuscontroller.Update).Methods("PUT")
-	r.HandleFunc("/aboutus", aboutuscontroller.Delete).Methods("DELETE")
+	r.HandleFunc("/aboutus/create", aboutuscontroller.Create).Methods("POST")
+	r.HandleFunc("/aboutus/update/{id}", aboutuscontroller.Update).Methods("PUT")
+	r.HandleFunc("/aboutus/delete", aboutuscontroller.Delete).Methods("DELETE")
 
 	r.HandleFunc("/products", productcontroller.Index).Methods("GET")
 	r.HandleFunc("/products/{id}", productcontroller.Show).Methods("GET")
-	r.HandleFunc("/product", productcontroller.Create).Methods("POST")
-	r.HandleFunc("/product/{id}", productcontroller.Update).Methods("PUT")
-	r.HandleFunc("/product", productcontroller.Delete).Methods("DELETE")
+	r.HandleFunc("/products/create", productcontroller.Create).Methods("POST")
+	r.HandleFunc("/products/update/{id}", productcontroller.Update).Methods("PUT")
+	r.HandleFunc("/products/delete", productcontroller.Delete).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
